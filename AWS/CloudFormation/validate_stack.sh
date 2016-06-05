adf715229010bb0970eb8cde068b8eabee903364
Karel Bemelmans <karel.bemelmans@nordcloud.se>
2016-06-05 20:57:42 +0200

    Added helper script that validate CF json files locally using aws cli

---
+++ AWS/CloudFormation/validate_stack.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# This is a small utility scrip that tests the validate of your CloudFormation
+# JSON files. It requires the aws cli, which you probably can install using your
+# computer's package manager (apt, yum, brew, ...)
+#
+# You also need to have configured your AWS credentals, currently they go in the
+# file $HOME/.aws/credentials
+#
+# An example config file looks like this:
+#
+# [default]
+# aws_access_key_id = XXX
+# aws_secret_access_key = YYY
+
+FAILED=0
+templates=( $(find . -type f -name "*.json") )
+
+for template in "${templates[@]}"
+do
+  aws cloudformation validate-template --template-body file://$template --region eu-west-1
+
+  if [ $? -eq 0 ]
+  then
+    echo "Successfully validated template: $template"
+  else
+    echo "Could not validate template: $template" >&2
+    FAILED=1
+  fi
+done
+
+exit $FAILED
