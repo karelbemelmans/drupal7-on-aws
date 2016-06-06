597c1b8d2ffcf86954f7ad923ec1557c9c409301
Karel Bemelmans <karel.bemelmans@nordcloud.se>
2016-06-06 09:53:08 +0200

    Updated copy to S3 script

--- AWS/copy_files_to_s3.sh
+++ AWS/copy_files_to_s3.sh
@@ -3,8 +3,11 @@
 # Small script to copy the files to my S3 bucket.
 # This will only work for me ofcourse.
 
-aws s3 cp . s3://karelbemelmans-running-drupal7-on-aws \
-  --region eu-west-1 \
-  --recursive \
-  --exclude "*.md" \
-  --exclude "*.sh"
+BUCKET=karelbemelmans-running-drupal7-on-aws/2016-06-06
+REGION=eu-west-1
+
+# Clean everything first
+aws s3 rm s3://$BUCKET --region $REGION --recursive
+
+# Copy the new files
+aws s3 cp . s3://$BUCKET --region $REGION --recursive --exclude "*.md" --exclude "*.sh"
